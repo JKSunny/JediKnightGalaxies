182d05b94461c14755852774db3c65534c66720c
Futuza <darthfutuza@hotmail.com>
2025-04-15 20:42:18 -0600

    improving MoveBin script for linux

--- tools/MoveBins-Linux.sh
+++ tools/MoveBins-Linux.sh
@@ -3,6 +3,19 @@ echo "moving files..."
 #change this location to the path of your build installation, note you might need sudo to create directories
 movedir=./release/
 
+#create necessary directories
+if [ ! -d $movedir ]; then
+	mkdir $movedir
+fi
+
+if [ ! -d $movedir"JKG/" ]; then
+	mkdir $movedir"/JKG/"
+fi
+
+if [ ! -d $movedir"JKG/server/" ]; then
+	mkdir $movedir"/JKG/server"
+fi
+
 #copy binaries
 cp -fr ./jkgalaxies.x86_64 $movedir"jkgalaxies.x86_64"
 cp -fr ./jkgalaxiesded.x86_64 $movedir"jkgalaxiesded.x86_64"
@@ -15,3 +28,7 @@ cp -fr ./codemp/ui/uix86_64.so $movedir"JKG/uix86_64.so"
 cd ../JKGalaxies/ && zip -FS -q -r ../build/zz_JKG_Assets5.pk3 * && cd ../build/
 cp -fr ./zz_JKG_Assets5.pk3 $movedir"JKG/zz_JKG_Assets5.pk3"
 echo "done"
+
+#copy over server content - may want to disable if testing server cfg
+cp -fr ../JKGServer/server/ $movedir"JKG/"
+cp ../JKGServer/server.cfg $movedir"/JKG/server.cfg"
